f666c52bfd0f283085ff64159b08476708b2389a
Dercio Anselmo <dercio@eventagrate.com>
2025-01-07 16:18:40 +0400

    components mapping correction

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 
-import Footer from "@/components/shared/footer";
+import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
 
 export default function RootLayout({
